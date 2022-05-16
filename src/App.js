38fcc7b311e0acb5c6c1d56a7797c07a59d3ec3b
Israel Furtado <yssrael@gmail.com>
2022-05-16 13:25:44 -0300

    v2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-
+import Login from './Login'
+import Professor from './Professor'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+          <Login />
+          <Professor />
     </div>
   );
 }
---
+++ src/Professor.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+
+
+class Professor extends Component{
+    render(){
+        return <div>
+            <form>
+                nome: <input type="text" />
+                <input type="submit" /> 
+            </form>
+        </div>
+   }
+}
+
+export default Professor;
+
